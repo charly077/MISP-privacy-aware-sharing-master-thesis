a1b6593f3024d27f49e9612ab5d18b9e6ff38bbd
charles jacquet <charles.jacquet7@gmail.com>
2016-11-17 12:25:18 -0500

    script to strip the number of rules to faster debug

--- encrypted_rules_implem/decrypt_frontend/remove_rules.py
+++ encrypted_rules_implem/decrypt_frontend/remove_rules.py
@@ -2,7 +2,7 @@ import os
 import re
 
 for name in os.listdir("rules"):
-    if int(re.findall("[0-9]+", name)[0]) > 100:
+    if int(re.findall("[0-9]+", name)[0]) > 1000:
         os.remove("rules/"+name)
 
 
