c5e30ac8fe7a9d1c341023544c12a7fc065c67c8
charles jacquet <charles.jacquet7@gmail.com>
2016-11-28 10:57:02 -0500

    start mysql backend

---
+++ encrypted_rules_implem/test_backend/readMisp.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# this code is inspired from https://github.com/CRIPTIM/private-IOC-sharing
+# which is using the MIT license
+from configuration import Configuration
+import requests, csv, json
+import argparse
+import argparse
+import configparser
+import glob
+import hashlib
+import re
+import subprocess
+import sys
+from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Util import Counter
+from hkdf import HKDF
+import os, shutil
+
+""" 
+Create the paper solution for Misp
+- read misp data
+- encrypt / hash them
+"""
+
+parser = argparse.ArgumentParser(description='Create an encrypted IOC \
+        rule.')
+parser.add_argument('--hash', dest='hash_name', default='sha256',
+        help='hash function to use')
+parser.add_argument('--iterations', type=int, default=1,
+        help='iterations needed before the decryption key is derived')
+
+args = parser.parse_args()
+
+conf = Configuration ()
+token = bytes(conf.misp_token, encoding='ascii')
+
+# first clean up folders
+if os.path.exists("rules"):
+    shutil.rmtree("rules")
+os.mkdir("rules")
+
+# update list is done via ./update.py
+IOCs = list()
+with open("res/misp_events.csv", "r") as f:
+    data = csv.DictReader(f)
+    for d in data:
+        IOCs.append(d)
+
+# create metadata
+meta = configparser.ConfigParser()
+meta['crypto'] = {}
+meta['crypto']['hash_name'] = args.hash_name
+meta['crypto']['dklen'] = str(16) # AES block size
+meta['crypto']['iterations'] = str(args.iterations)
+with open('rules/metadata', 'w') as config:
+    meta.write(config)
+
+
+def create_rule(ioc, message):
+    # encrypt the ioc and the message
+    salt = Random.new().read(hashlib.new(args.hash_name).digest_size)
+    dklen = 16 # AES block size
+    iv = Random.new().read(16) # TODO they use it but check if secure
+
+    # Spit + redo allow to ensure the same order to create the password
+    attr_types = '||'.join(attr_type for attr_type in ioc)
+    password = '||'.join(ioc[attr_type] for attr_type in ioc)
+
+    # encrypt the message
+    if args.iterations == 1:
+        kdf = HKDF(args.hash_name)
+        kdf.extract(salt, password.encode("utf8"))
+        dk = kdf.expand(info=token, L=dklen)
+    else:
+        dk = hashlib.pbkdf2_hmac(args.hash_name, password.encode('utf8'), salt, args.iterations, dklen=dklen)
+
+    ctr = Counter.new(128, initial_value=int.from_bytes(iv, 'big'))
+    cipher = AES.new(dk, AES.MODE_CTR, b'', counter=ctr)
+    ciphertext = cipher.encrypt(b'\x00'*16 + message.encode('utf-8'))
+
+    # store the rules 
+    rule = {}
+    rule['salt'] = b64encode(salt).decode('ascii')
+    rule['attributes'] = attr_types
+    rule['iv'] = b64encode(iv).decode('ascii')
+    rule['ciphertext'] = b64encode(ciphertext).decode('ascii')
+
+    return rule
+
+# message = information that we get when there is a match
+def create_message(attr):
+    uuid = attr["uuid"]
+    event_id = attr["event_id"]
+    date = attr["date"]
+    return "{}:{}:{}".format(uuid, event_id, date)
+
+def parse_attribute(attr):
+    # an attribute can have either one type or a list of type
+    split_type = attr["type"].split('|')
+    ioc = {}
+    if (len(split_type)>1):
+        # more than one value
+        split_value = attr["value"].split('|')
+        for i in range(len(split_type)):
+            ioc[split_type[i]] = split_value[i]
+    else:
+        ioc[attr["type"]] = attr["value"]
+    msg = create_message(attr)
+    return create_rule(ioc, msg)
+
+
+iocs = [parse_attribute(ioc) for ioc in IOCs]
+# sort iocs in different files for optimization
+iocDic = {}
+for ioc in iocs:
+    typ = "_".join(ioc["attributes"].split('||'))
+    try:
+        iocDic[typ].append(ioc)
+    except:
+        iocDic[typ] = [ioc]
+
+for typ in iocDic:
+    with open('rules/'+ typ +'.csv', 'wt') as output_file:
+        dict_writer = csv.DictWriter(output_file, iocDic[typ][0].keys(), delimiter='\t')
+        dict_writer.writeheader()
+        dict_writer.writerows(iocDic[typ])
---
+++ encrypted_rules_implem/test_backend/update.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from configuration import Configuration
+import requests
+import json
+import os, shutil
+
+conf = Configuration()
+
+# first let clean the ressources
+if os.path.exists("res"):
+    shutil.rmtree("res")
+os.mkdir("res")
+
+# update false positive list : https://github.com/MISP/misp-warninglists
+# and get updated values
+def save_json(url, name, remove_point=False, add_www=False):
+    req = requests.get(url)
+    l = req.json()['list']
+    l = [val.lower() for val in l]
+    if remove_point:
+        list2 = [val[1:] for val in l if val.startswith('.')]
+        list2.extend([val for val in l if not val.startswith('.')])
+        l = list2
+    if add_www:
+        list2 = ['www.%s' % val for val in l]
+        l.extend(list2)
+    json_list = {'list': l}
+    with open('res/{}.json'.format(name), 'w+') as f:
+        json.dump(json_list, f)
+
+# List of the top level domains
+save_json(conf.tlds, 'tlds')
+
+# list of the most visited web pages (alexa) with www. added
+save_json(conf.alexa, 'alexa', add_www=True)
+
+# list of empty hashes
+save_json(conf.empty_hash, 'empty_hash')
+
+# list of google domains (without the firs .)
+save_json(conf.google, 'google', remove_point=True)
+
+# list of ip multicast
+save_json(conf.ip_multicast, 'ip_multicast')
+
+# list of public dns
+save_json(conf.public_dns_v4, 'public_dns_v4')
+save_json(conf.public_dns_v6, 'public_dns_v6')
+
+# lists of usual internal network ip
+save_json(conf.ip_rfc1918, 'ip_rfc1918')
+save_json(conf.ip_rfc5737, 'ip_rfc5737')
+
+# list of second level domains
+save_json(conf.second_domain, 'second_domain')
+
+# get ad_domain to avoid
+ad_req = requests.get("https://pgl.yoyo.org/as/serverlist.php?showintro=0;hostformat=hosts")
+ad_lines = ad_req.text.split('\n')
+ad_lines = [line for line in ad_lines if line.startswith("127")]
+ad_list = [line.split(" ")[1] for line in ad_lines]
+
+ad_json = {'list': ad_list, 'name': 'advertisement domains', 'version': 0,
+           'description': 'This list comes from https://pgl.yoyo.org/as/serverlist.php?showintro=0;hostformat=hosts',
+           'matching_attributes': ["hostname", "domain", "domain|ip"]}
+with open('res/ad.json', 'w+') as f:
+    json.dump(ad_json, f)
+
+
+
+# get misp data in csv
+session = requests.Session()
+session.verify = True
+session.proxies = None
+
+header = {}
+header['Authorization'] = conf.misp_token
+session.headers.update(header)
+
+# Change to csv (only download ids elements!)
+events = session.get('{}events/csv/download/'.format(conf.misp_url))
+
+with open('res/misp_events.csv', 'w+') as f:
+    f.write(events.text.encode("utf8"))
