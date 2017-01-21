81f9391730fe6290212fd75deede4df0e4e82292
Charles Jacquet <charles.jacquet7@gmail.com>
2017-01-21 16:33:11 +0100

    add some files for benchmarking against exhaustive search on ips

---
+++ encrypted_rules_implem/helpers.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+read/write misp database for testing purpose
+"""
+from encrypt_backend.configuration import Configuration as e_conf
+# mysql import
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.schema import MetaData, Table
+from sqlalchemy.sql import select
+
+class DatabaseHelper:
+	# connection
+	def __init__(self):
+		Base = automap_base()
+	    engine = create_engine('mysql://{}:{}@{}/{}'.format(e_conf.user, e_conf.password, e_conf.host, e_conf.dbname))
+
+	    Base.prepare(engine, reflect=True)
+	    metadata = MetaData()
+	    metadata.reflect(bind=engine)
+	    self.connection = engine.connect()
+	    self.attributes_table = Table("attributes", metadata, autoload=True)
+		
+	# close database
+	def closedb():
+		self.connection.close()
+
+	# Save all attr in database as a tsv file
+	def saveAttr(self, attr_name):
+		pass
+
+	# Restore all attr in database from tsv file 
+	def restoreAttr(self, attr_file):
+		pass
+
+	# add attr
+	def addAttr(self, attr, attr_type):
+		pass
