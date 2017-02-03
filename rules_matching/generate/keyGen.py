027fe0ff5a8c23fef8557ec92719c6c9ab48a603
charles jacquet <charles.jacquet@conostix.com>
2017-02-03 14:45:48 +0100

    Continue search

---
+++ rules_matching/generate/keyGen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Just try to implement some other key generation system
+for benchmarking
+"""
+
+# need to change the name
+Class Derivator():
+    """
+    Each function need to generate a key from attribute/token/salt
+    """
+
