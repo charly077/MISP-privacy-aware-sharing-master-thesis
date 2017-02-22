01747f209d3cde0ef7961bd6f7bdcea9559313ad
charles jacquet <charles.jacquet7@gmail.com>
2017-02-22 16:26:16 +0100

    horrible mistake git add choose_crypto.py git add choose_crypto.py

--- privacy_sharing/src/crypto/choose_crypto.py
+++ privacy_sharing/src/crypto/choose_crypto.py
@@ -2,6 +2,6 @@ def Crypto(name, conf, metadata=None):
     if (name == 'pbkdf2'):
         from crypto.pbkdf2 import Pbkdf2
         return Pbkdf2(conf, metadata)
-    else if (name == 'bcrypt'):
+    elif (name == 'bcrypt'):
         from crypto.bcrypt import Bcrypt
         return Bcrypt(conf, metadata)
