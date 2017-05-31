3f480433c6a3bb62a674ab2c01d0f00a0bd30729
Charles Jacquet <charles.jacquet7@gmail.com>
2017-05-31 21:06:05 +0200

    Update benchmark.py

--- privacy_sharing/src/benchmark.py
+++ privacy_sharing/src/benchmark.py
@@ -15,25 +15,6 @@ def kdf():
     kdf.start()
 
 
-# Simple execution like in the article
-
-## Lookup in function of the number of contained IP
-"""Linear"""
-
-## Rules space in function of the number of Elements (IP, ..)
-"""Only depend on the message size but Linear"""
-
-## Bruteforcing range ip (/24)
-### Reactions in function of the number of IPs
-"""Linear as the size of an IP is equivalent"""
-
-
-### Reactions in function on the number of iterations
-""""Linear for the same reason"""
-
-## Time to brute force increasing size elements
-"""Should be exponential"""
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run tests')
     parser.add_argument('-a', '--all', dest='all', action='store_true', help='Runs all benchmarks')
